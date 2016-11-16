59baaa820d5368deafdb0eba042b932e24f4ebf4
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2016-11-16 10:30:02 +0100

    All fields now link to the item

--- parts/record-loop.php
+++ parts/record-loop.php
@@ -4,12 +4,14 @@
 			<?php
 				while (have_rows('authors')) : the_row();
 					echo '<li>';
+					echo '<a href="' . get_permalink() . '">';
 					the_sub_field('author');
+					echo '</a>';
 					echo '</li>';
 				endwhile;
 			?>
 		</ul>
 	</td>
 	<td><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></td>
-	<td><?php the_field('year'); ?></td>
+	<td><a href="<?php the_permalink(); ?>"><?php the_field('year'); ?></a></td>
 </tr>
